01abe444b06aa30111a0e0309ba064e23d9961d5
Kundan Kumar <kundan29304@gmail.com>
2025-10-06 23:51:03 +0530

    findAverage of array

---
+++ complete_Java_Dsa/9.Average.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
+        System.out.println("Enter  the  size of the  Arry:");
+        int n = sc.nextInt();
+        int arr[] = new  int[n];
+        int k = 5;
+
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int [] result = new posAverage(arr);
+        System.out.println(Arrays.toString(result));
+
+    }
+    public static double posAverage(int[] arr) {
+        double sum = 0;
+        int count = 0;
+
+        for (int num : arr) {
+            if (num > 0) {
+                sum += num;
+                count++;
+            }
+        }
+
+        if (count == 0) return 0.0; // No positive numbers
+        return sum / count;
+    }
+}
